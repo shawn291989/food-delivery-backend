65700cf61cdf708775a9c5ebd5f178c75b8d2585
anwarulislamshawn <40534316+anwarulislamshawn@users.noreply.github.com>
2022-08-19 00:27:49 +0600

    hotfix

--- src/database/seeds/restaurant.seed.ts
+++ src/database/seeds/restaurant.seed.ts
@@ -1,23 +1,33 @@
 import { Factory, Seeder } from 'typeorm-seeding';
-import { Connection } from 'typeorm';
+import { Connection, getConnection, getRepository } from 'typeorm';
 import { Restaurant } from '../../restaurents/entities/restaurants.entity';
 import * as restaurants from '../data/restaurant-data.json';
+import { stringify } from 'querystring';
+import { Menu } from '../../restaurents/menu/entities/menu.entity';
+import { RestaurantService } from '../../restaurents/restaurants.service'
+import { RestaurantRepository } from '../../restaurents/restaurants.repository';
+import { InjectRepository } from '@nestjs/typeorm';
 
 export default class CountrySeed implements Seeder {
+  constructor(
+    @InjectRepository(RestaurantRepository)
+    private restauranRepository: RestaurantRepository) { }
   public async run(factory: Factory, connection: Connection): Promise<any> {
+
     await Promise.all(
       restaurants.map(async (item) => {
+        const outString = item.restaurantName.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '');
         const restaurant = {
-          restaurantName: item.restaurantName,
+          restaurantName: outString,
           cashBalance: item.cashBalance,
         };
-
         await connection
           .createQueryBuilder()
           .insert()
           .into(Restaurant)
           .values(restaurant)
           .execute();
+
       }),
     );
   }
